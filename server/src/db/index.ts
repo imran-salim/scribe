90c899dc1abd3f850b88e097e3384b6858d63106
i8abyte <imransalim08@gmail.com>
2026-02-25 12:09:26 +1300

    Add a log to determine which host the server is trying to connect to

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -4,6 +4,13 @@ import * as schema from "./schema.js";
 
 const { Pool } = pg;
 
+if (process.env.DATABASE_URL) {
+  const url = new URL(process.env.DATABASE_URL);
+  console.log(`DB Connection: Attempting to connect to host "${url.host}"`);
+} else {
+  console.error("DB Connection: DATABASE_URL is NOT set!");
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL?.includes("localhost") ? false : { rejectUnauthorized: false },
